d7795be0f41631a73ce0909416c46701f9d2f23a
Elena <elenapetrovich28@gmail.com>
2021-04-23 00:21:27 +0500

    Модуль кабинет

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,63 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
-import string
-import random
-from django.db.models import Count
 from django.contrib.auth.backends import ModelBackend
 
-
-class Company(models.Model):
-    name = models.CharField("Название", max_length=254)
-    city = models.CharField("Город", max_length=254)
-    address = models.CharField("Адрес", max_length=254)
-    email = models.EmailField("Электронная почта")
-    mongodb_collection = models.CharField("collection", max_length=254)
-    public = models.BooleanField("Публичность", default=False)
-    secret_key = models.CharField("Ключ доступа", max_length=254)
-    password = models.CharField("Пароль", max_length=254)
-
-    def __str__(self) -> str:
-        return f"{self.name} г. {self.city}"
-
-
-class Role(models.Model):
-    group = models.OneToOneField(Group, on_delete=models.CASCADE)
-    company = models.ForeignKey("cabinet.Company",
-                                on_delete=models.CASCADE,
-                                null=True,
-                                related_name="roles")
-
-    def __str__(self) -> str:
-        return f"{self.group} ({self.company})"
+from company.models import Collection, Role, Company
 
 
 class Account(User):
     class Meta:
         proxy = True
 
-    def get_roles(self, company: str = None, group: str = None):
+    def get_roles(self, company: str = None, role: str = None):
         if company is None:
-            return Role.objects.filter(group__user=self).all()
-        elif group is None:
-            return Role.objects.filter(group__user=self,
-                                       company__name=company).all()
+            return Role.objects.filter(users=self).all()
+        elif role is None:
+            return Role.objects.filter(users=self, system_name=company).all()
         else:
-            return Role.objects.filter(group__user=self,
-                                       company__name=company,
-                                       group__name=group).all()
+            return Role.objects.filter(users=self,
+                                       system_name=company,
+                                       name=role).all()
 
-    def get_companies(self, company: str = None, group: str = None):
+    def get_companies(self, company: str = None, role: str = None):
         if company is None:
-            return Company.objects.filter(
-                roles__group__user=self).distinct().all()
-        elif group is None:
-            return Company.objects.filter(
-                name=company, roles__group__user=self).distinct().all()
+            return Company.objects.filter(roles__users=self).distinct().all()
+        elif role is None:
+            return Company.objects.filter(system_name=company,
+                                          roles__users=self).distinct().all()
         else:
-            return Company.objects.filter(
-                name=company,
-                roles__group__user=self,
-                roles__group__name=group).distinct().all()
+            return Company.objects.filter(system_name=company,
+                                          roles__users=self,
+                                          roles__name=role).distinct().all()
+
+    def get_collection(self, company: str = None, collection: str = None):
+        return Company.objects.filter(system_name=company,
+                                      roles__users=self).distinct().all()
 
 
 class AccountModelBackend(ModelBackend):
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets, mixins, views, status
-from rest_framework.decorators import api_view, action
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import permissions
 
-from .models import Account, Company
-from .serializers import AccountSerializer, CompanySerializer, GroupSerializer, RoleSerializer
+from .models import Account
+from .serializers import AccountSerializer
+from company.serializers import RoleSerializer
 
 
 class UserViewSet(viewsets.GenericViewSet):
@@ -23,24 +23,6 @@ class UserViewSet(viewsets.GenericViewSet):
             "roles": roles_data
         },
                         template_name="user.html")
-
-
-class CompanyViewSet(viewsets.GenericViewSet):
-    queryset = Company.objects
-    serializer_class = CompanySerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.queryset.filter().all())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({"company_list": serializer.data},
-                        template_name="company_list.html")
 
 
 @api_view(['GET', 'POST'])
